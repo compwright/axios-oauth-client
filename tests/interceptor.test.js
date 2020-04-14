3e9cb28bf4f1c8f7311b1f144a944eefb9f43f07
Jonathon Hill <jhill9693@gmail.com>
2020-04-14 14:48:10 -0400

    chore(build): Build to ES5

--- tests/interceptor.test.js
+++ tests/interceptor.test.js
@@ -2,54 +2,54 @@ const assert = require('assert');
 const tokenProvider = require('axios-token-interceptor');
 const interceptor = require('../src/interceptor');
 
-describe('interceptor', function() {
-  it('should return an axios request interceptor function', function() {
+describe('interceptor', function () {
+  it('should return an axios request interceptor function', function () {
     assert.strictEqual(typeof interceptor(tokenProvider), 'function');
   });
 
-  describe('the axios request interceptor function', function() {
-    it('should set the oauth access token in the request authorization header with the bearer scheme', async function() {
-      const authenticate = async() => ({
+  describe('the axios request interceptor function', function () {
+    it('should set the oauth access token in the request authorization header with the bearer scheme', async function () {
+      const authenticate = async () => ({
         access_token: 'foo',
         expires_in: 0
       });
       const newRequest = await interceptor(tokenProvider, authenticate)({
         headers: {}
       });
-      assert.deepEqual(newRequest, {
+      assert.deepStrictEqual(newRequest, {
         headers: {
           Authorization: 'Bearer foo'
         }
       });
     });
 
-    it('should cache the oauth access token until it expires', async function() {
-      function delay(ms) {
+    it('should cache the oauth access token until it expires', async function () {
+      function delay (ms) {
         return new Promise((resolve, reject) => setTimeout(resolve, ms));
       }
 
       let calls = 0;
-      const authenticate = async() => {
+      const authenticate = async () => {
         return {
           access_token: 'foo' + calls++,
           expires_in: 2
-        }
+        };
       };
 
       const req = { headers: {} };
       const requestInterceptor = interceptor(tokenProvider, authenticate);
 
-      function test3x(req) {
+      function test3x (req) {
         return Promise.all([
           requestInterceptor(req),
           requestInterceptor(req),
           requestInterceptor(req)
-        ])
+        ]);
       }
 
-      function verify(results, expected) {
-        for (let result of results) {
-          assert.deepEqual(result, expected);
+      function verify (results, expected) {
+        for (const result of results) {
+          assert.deepStrictEqual(result, expected);
         }
       }
 
