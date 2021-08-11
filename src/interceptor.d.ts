7f60df5b147f5565cf67f9146756a07da79e9d35
E-gy <balakhelisey@gmail.com>
2021-08-11 19:40:39 +0200

    🧹 2 space tabulation; 80/100 word wrap (inc jsdoc)

--- src/interceptor.d.ts
+++ src/interceptor.d.ts
@@ -1,6 +1,9 @@
-import { AxiosRequestConfig } from 'axios';
-import AxiosTokenProvider from 'axios-token-interceptor';
-import ClientFactory from './client';
+import { AxiosRequestConfig } from "axios";
+import AxiosTokenProvider from "axios-token-interceptor";
+import ClientFactory from "./client";
 
-declare function InterceptorFactory(tokenProvider: AxiosTokenProvider, authenticate: ClientFactory): AxiosTokenProvider.TokenProvider;
+declare function InterceptorFactory(
+  tokenProvider: AxiosTokenProvider,
+  authenticate: ClientFactory
+): AxiosTokenProvider.TokenProvider;
 export = InterceptorFactory;
